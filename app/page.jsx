c7562ba22a4d49be99b4fb0519d5dbdac11036d0
Dodi Yulian <cdoditri@gmail.com>
2024-02-23 10:15:05 +0700

    homepage components

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import Footer from "@/components/Footer";
 
 const HomePage = () => {
   console.log("test");
   return (
     <div>
-      <h1 className="text-lg font-semibold">Home Page</h1>
-      <Link href="/properties">Properties</Link>
+      <Hero />
+      <InfoBoxes />
+      <Footer />
     </div>
   );
 };
